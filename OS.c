5b94a67ef0312d77005808816efacb6bdf7fe5d1
Chumphrey8 <ch410023@ohio.edu>
2026-09-09 15:06:35 -0400

    done
    code done

---
+++ OS.c
@@ -0,0 +1,157 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 80
+
+/**
+ * setup() reads in the next command line, separating it into
+ * distinct tokens using whitespace as delimiters.
+ *
+ * setup() modifies the args parameter so that it holds pointers
+ * to the null-terminated strings that are the tokens in the most
+ * recent user command line as well as a NULL pointer, indicating
+ * the end of the argument list.
+ */
+void setup(char inputBuffer[], char *args[], int *background)
+{
+    int length,       /* # of characters in the command line */
+        i,            /* loop index for accessing inputBuffer array */
+        start,        /* index where beginning of next command parameter is */
+        ct;            /* index of where to place the next parameter into args[] */
+
+    ct = 0;
+
+    /* Read what the user enters on the command line */
+    length = read(STDIN_FILENO, inputBuffer, MAX_LINE);
+
+    start = -1;
+
+    if (length == 0) {
+        /* Ctrl-D was entered, end of user command stream */
+        exit(0);
+    }
+
+    if (length < 0) {
+        perror("Error reading the command");
+        exit(-1);
+    }
+
+    /* Examine every character in the inputBuffer */
+    for (i = 0; i < length; i++) {
+
+        switch (inputBuffer[i]) {
+
+            case ' ':
+            case '\t':
+                /* Argument separators */
+                if (start != -1) {
+                    args[ct] = &inputBuffer[start];
+                    ct++;
+                }
+
+                inputBuffer[i] = '\0';
+                start = -1;
+                break;
+
+            case '\n':
+                /* Newline marks the end of the command */
+                if (start != -1) {
+                    args[ct] = &inputBuffer[start];
+                    ct++;
+                }
+
+                inputBuffer[i] = '\0';
+
+                /* No more arguments */
+                args[ct] = NULL;
+                break;
+
+            case '&':
+                /* Command should run in the background */
+                *background = 1;
+                inputBuffer[i] = '\0';
+                break;
+
+            default:
+                /* Some other character */
+                if (start == -1) {
+                    start = i;
+                }
+        }
+    }
+
+    /* Just in case the input line was greater than MAX_LINE */
+    args[ct] = NULL;
+}
+
+int main(void)
+{
+    char inputBuffer[MAX_LINE];          /* Buffer to hold command entered */
+    int background;                      /* 1 if command is followed by '&' */
+    char *args[MAX_LINE / 2 + 1];       /* Command line arguments */
+
+    while (1) {
+
+        /* Reset background flag for each new command */
+        background = 0;
+
+        /* Display the shell prompt */
+        printf("COMMAND->");
+        fflush(stdout);
+
+        /*
+         * setup() reads the user's command and
+         * fills the args array.
+         */
+        setup(inputBuffer, args, &background);
+
+        /*
+         * Create a child process.
+         */
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            /*
+             * fork() failed.
+             */
+            perror("Fork failed");
+            exit(1);
+        }
+
+        if (pid == 0) {
+            /*
+             * Child process.
+             *
+             * Execute the command entered by the user.
+             * args[0] contains the command name and
+             * args contains all of the command arguments.
+             */
+            if (execvp(args[0], args) == -1) {
+                perror("Command execution failed");
+                exit(1);
+            }
+        }
+        else {
+            /*
+             * Parent process.
+             *
+             * If background == 0, this is a foreground
+             * process, so wait for the child to finish.
+             *
+             * If background == 1, this is a background
+             * process, so continue immediately without
+             * waiting for the child.
+             */
+            if (background == 0) {
+                wait(NULL);
+            }
+        }
+    }
+
+    return 0;
+}
+
